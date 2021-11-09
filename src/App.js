6b2ccb4841959388e4102fe1ca5c995a338ef4f4
rust_man <gasymovrustam86@yandex.com>
2021-11-09 23:20:01 +0200

    added function getTodos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Todos from "./components/todos/Todos";
 import AddTodo from "./components/addTodo/AddTodo";
-import { useTodos } from "./hooks/useTodos";
 import { useSelector } from "react-redux";
+import { todosState } from "./toolkit/todosSlice";
 function App() {
-  const [todos] = useTodos();
   const todosData = useSelector(todosState);
   return (
     <>
       <div className="container">
         <h1 className="title">Task Manager</h1>
-        {todosData.error && <h2 className={cn.partName}>Error: {todosData.error}</h2>}
-        {todos.length === 0 && (
-          <h2 className="content-insription">You don`t have any tasks right now!</h2>
+        {todosData.error ? (
+          <h2 className="content-insription">Error: {todosData.error}</h2>
+        ) : (
+          todosData.todos.length === 0 && (
+            <h2 className="content-insription">You don`t have any tasks right now!</h2>
+          )
         )}
         <div className="general-wrapper">
           <Todos status="0" />
--- src/api/todos.js
+++ src/api/todos.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:12314",
+  baseURL: "http://localhost:1234",
   headers: { "Content-type": "application/json;charset=utf-8" },
 });
 
